36db5ad88b1b2c5a5859b18682dc3b09f4a99c7f
Titof <lanyanfockchristophe@gmail.com>
2024-01-09 18:46:14 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,7 @@ struct AppState {
 struct Registry {
     images: Vec<Image>,
     blobs: Vec<Blobs>,
+    manifests: Vec<Manifests>,
     request_chunk_uploads: Vec<RequestChunkUpload>
 }
 
@@ -45,6 +46,14 @@ struct Image {
     tag: String,
     blobs_digest: Vec<String>
 }
+
+#[derive(Debug)]
+struct Manifests {
+    digest: String,
+    data: Vec<u8>
+}
+
+
 #[derive(Debug)]
 struct Blobs {
     digest: String,
@@ -53,10 +62,12 @@ struct Blobs {
 #[derive(Debug, Clone)]
 struct RequestChunkUpload {
     session_id: Uuid,
-    close_sesssion_id: Uuid,
+    close_session_id: Uuid,
     date: Duration,
     data: Vec<u8>,
 }
+const ip: &str= "http://192.168.50.108:3000";
+// const ip: &str= "http://172.26.16.1:3000";
 
 #[tokio::main]
 async fn main() {
@@ -73,15 +84,15 @@ async fn main() {
         .route("/v2/:name/blobs/uploads/", post(upload_blob))
         .route(
             "/v2/:name/blobs/uploads/:reference",
-            put(upload_blob_with_reference),
+            put(put_upload_blob),
         )
         .route(
             "/v2/:name/blobs/uploads/:reference",
             patch(patch_upload_blob),
         )
-        .route("/v2/:name/manifests/:reference", get(with_status))
+        .route("/v2/:name/manifests/:reference", get(get_manifest))
         .route("/v2/:name/manifests/:reference", head(manifest_exists))
-        .route("/v2/:name/manifests/:reference", put(with_status))
+        .route("/v2/:name/manifests/:reference", put(put_upload_manifest))
         .route("/v2/:name/manifests/:reference", delete(with_status))
         .route("/v2/:name/tags/list", get(with_status))
         // end-1 S: 200, F: 404/401
@@ -111,6 +122,7 @@ async fn main() {
                     info!("{} {:?}", _request.uri(), _request.headers());
                 })
                 .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
+                    info!("Response status {} {:?}", _response.status(), _response.headers());
                     // ...
                 })
                 .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
@@ -178,6 +190,63 @@ async fn get_blob(
         .unwrap()
     }
 }
+
+
+async fn get_manifest(
+    Path((_name, digest)): Path<(String, String)>,
+    State(state): State<SharedState>,
+) -> Response {
+    let resp = Response::builder();
+    let registry = &state.read().unwrap().registry;
+    let blob = registry.manifests.iter().find(|x| x.digest == digest);
+    println!("{:?}", registry.manifests.iter().map(|x| x.digest.clone()).collect::<Vec<String>>());
+
+    if blob.is_none() {
+        resp
+        .status(StatusCode::NOT_FOUND)
+        .body(Body::empty())
+        .unwrap()
+    } else {
+        let data = blob.unwrap().data.to_vec();
+        resp
+        .status(StatusCode::OK)
+        .header("content-type", "application/vnd.docker.distribution.manifest.v2+json")
+        .body(Body::from(data))
+        .unwrap()
+    }
+}
+
+async fn put_upload_manifest(
+    Path((name, reference)): Path<(String, String)>,
+    headers: HeaderMap,
+    State(state): State<SharedState>,
+    body: Bytes,
+) -> Response {
+    let registry = &mut state.write().unwrap().registry;
+    let sha = digest(body.clone().to_vec());
+    let digest = format!("sha256:{}",sha);
+    let blob = registry.manifests.iter().find(|x| x.digest == digest);
+    if blob.is_none() {
+        let _ = &mut registry.manifests.push(Manifests{
+            digest: digest.clone(),
+            data: body.clone().to_vec()
+        });
+    }
+
+    println!("{}", String::from_utf8_lossy(body.to_vec().as_slice()));
+
+    Response::builder()
+            .header("content-type", "application/json")
+            .header("Docker-Content-Digest", digest.clone())
+            .header("Location", format!(
+                "{}/v2/{}/manifest/{}",
+                ip, name,digest
+            ))
+            .status(StatusCode::CREATED)
+            .body(Body::empty())
+            .unwrap()
+}
+
 
 
 
@@ -265,13 +334,57 @@ pub async fn patch_upload_blob_with_reference(
         .header(
             "Location",
             format!(
-                "http://172.26.16.1:3000/v2/{}/blobs/uploads/{}",
-                name, my_uuid
+                "{}/v2/{}/blobs/uploads/{}",
+                ip, name, my_uuid
             ),
         )
         .status(StatusCode::ACCEPTED)
         .body(Body::from(""))
         .unwrap()
+}
+
+
+async fn put_upload_blob(
+    Path((name, reference)): Path<(String, String)>,
+    headers: HeaderMap,
+    Query(digest_payload): Query<DigestPayload>,
+    State(state): State<SharedState>,
+    body: Bytes,
+) -> Response {
+    let registry = &mut state.write().unwrap().registry;
+    let chunk = &mut registry.request_chunk_uploads.iter_mut().find(|chunck_upload| chunck_upload.close_session_id.to_string().eq(&reference) || chunck_upload.session_id.to_string().eq(&reference));
+    if chunk.is_none() {
+        return Response::builder()
+        .header("content-type", "application/json")
+        .status(StatusCode::BAD_REQUEST)
+        .body(Body::empty())
+        .unwrap();
+    }
+    if !headers.get("content-length").is_none() {
+        let content_length = headers.get("content-length").unwrap().to_str().unwrap().parse::<i32>().unwrap();
+        if content_length > 0 {
+            chunk.as_mut().unwrap().data.extend(body.to_vec());
+        }
+        println!("Digest {:?}", digest_payload.digest);
+        if digest_payload.digest.is_some() {
+            registry.blobs.push(Blobs { digest: digest_payload.digest.clone().unwrap(), data: chunk.as_ref().unwrap().data.clone() })
+        }
+        return Response::builder()
+        .header("location", format!(
+            "{}/v2/{}/blobs/{}",
+            ip, name,digest_payload.digest.unwrap()
+        ))
+        .status(StatusCode::CREATED)
+        .body(Body::empty())
+        .unwrap();
+    }
+
+
+    Response::builder()
+            .header("content-type", "application/json")
+            .status(StatusCode::BAD_REQUEST)
+            .body(Body::empty())
+            .unwrap()
 }
 
 
@@ -298,12 +411,21 @@ async fn patch_upload_blob(
                 .body(Body::from(json_payload))
                 .unwrap();
     }
-    println!("Headers: {:?}", headers);
     if !headers.get("content-length").is_none() {
+        println!("*********************************");
         let content_length = headers.get("content-length").unwrap().to_str().unwrap().parse::<i32>().unwrap();
         if content_length > 0 {
             chunck.as_mut().unwrap().data.extend(body.to_vec());
         }
+        return Response::builder()
+        .header("range", format!("0-{}",body.to_vec().len()))
+        .header("location", format!(
+            "{}/v2/{}/blobs/uploads/{}",
+            ip, name, chunck.as_ref().unwrap().close_session_id
+        ))
+        .status(StatusCode::ACCEPTED)
+        .body(Body::empty())
+        .unwrap();
     }
     Response::builder()
     .header("content-type", "application/json")
@@ -328,15 +450,15 @@ async fn upload_blob(
             .expect("Time went backwards");
         let request_chunck_upload = RequestChunkUpload {
             session_id: Uuid::new_v4(),
-            close_sesssion_id: Uuid::new_v4(),
+            close_session_id: Uuid::new_v4(),
             date: since_the_epoch,
             data: vec![],
         };
         registry.registry.request_chunk_uploads.push(request_chunck_upload.clone());
         return Response::builder()
         .header("Location", format!(
-            "http://172.26.16.1:3000/v2/{}/blobs/uploads/{}",
-            name, request_chunck_upload.session_id
+            "{}/v2/{}/blobs/uploads/{}",
+            ip, name, request_chunck_upload.session_id
         ))
         .status(StatusCode::ACCEPTED)
         .body(Body::from(""))
@@ -373,8 +495,8 @@ pub async fn upload_blob_old(
             .header(
                 "Location",
                 format!(
-                    "http://172.26.16.1:3000/v2/{}/blobs/uploads/{}",
-                    name, my_uuid
+                    "{}/v2/{}/blobs/uploads/{}",
+                    ip, name, my_uuid
                 ),
             )
             .body(Body::empty())
